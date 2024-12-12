8822b8eeef991773a67674910f6fddb7ef2eb51d
ajzerpa <armando_zerpa@hotmail.com>
2024-12-12 21:24:37 +0100

    first commit

---
+++ CodeProblems/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CodeProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            TwoSum c = new TwoSum();
+            int[] res = c.solution(new int[] { 3, 2, 4 }, 6);
+            Console.WriteLine($"Result: {res[0]}, {res[1]}");
+            res = c.solution(new int[] { 2, 7, 11, 15 }, 9);
+            Console.WriteLine($"Result: {res[0]}, {res[1]}");
+            Console.ReadKey();
+        }
+    }
+}
+
