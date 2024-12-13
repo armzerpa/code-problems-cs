f075b889319c868c9278fd66775173e2098ead58
ajzerpa <armando_zerpa@hotmail.com>
2024-12-13 21:59:51 +0100

    contain duplicates

---
+++ CodeProblems/ContainDuplicate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeProblems
+{
+    public class ContainDuplicate
+    {
+        public bool containDuplicate(int[] nums)
+        {
+            HashSet<int> uniqueElements = new HashSet<int>();
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (uniqueElements.Contains(nums[i]))
+                {
+                    return true;
+                }
+                uniqueElements.Add(nums[i]);
+            }
+            return false;
+        }
+    }
+}
+
