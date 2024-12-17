1f9f3afd083f62f233cead675d8177ad5c10b841
ajzerpa <armando_zerpa@hotmail.com>
2024-12-17 22:15:34 +0100

    Move zeros

---
+++ CodeProblems/MoveZeros.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeProblems
+{
+    public class MoveZeros
+    {
+        public void moveZeros(int[] nums)
+        {
+            int nonZeroIndex = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    nums[nonZeroIndex] = nums[i];
+                    nonZeroIndex++;
+                } 
+            }
+
+            for (int i = nonZeroIndex; i < nums.Length; i++)
+                nums[i] = 0;
+        }
+       
+    }
+}
+
